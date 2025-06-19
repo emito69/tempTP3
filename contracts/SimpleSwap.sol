3c29e13e0313a673204a13e9b36c99f5471c60a1
emito69 <alvarezemiliano@gmail.com>
2025-06-19 14:32:32 -0300

    commit 2

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -4,46 +4,52 @@ pragma solidity ^0.8.27;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./LiquidityToken.sol";
+import "hardhat/console.sol";
 
 //interface MintableERC20 is LiquidityToken {
 //    function mint(address to, uint256 amount) external;
 //}
 
-    struct Person{
-        address id;
-        uint256 amount1;  
-        uint256 amount2;  
-        uint256 liquidity;  
-    }
 
+// VER LOS REQUIREs Y EVENTOS
+// CORREGIRr por cantidad de decimales
 
 contract SimpleSwap is Ownable {
 
-    IERC20 public token1;
-    IERC20 public token2;
     LiquidityToken public liquidityToken;
     //uint256 public price;
 
-    constructor(address _token1, address _token2) Ownable(msg.sender) {
-        token1 = IERC20(_token1);
-        token2 = IERC20(_token2);
+    mapping (address => uint256) public personLiquidity;  
+
+    mapping (address => IERC20) public tokensData;  
+
+    //@notice: Mapping to check if Token already exists in the contrac 
+    mapping (address => bool) public isToken; // default `false`
+
+    constructor() Ownable(msg.sender) {
         liquidityToken = new LiquidityToken(address(this));
-
     }
-
 
 
 /**** FUNCIONES EXTERNAS ****/
 
     // 0 PRUEBA
 
-    function prueba(uint256 _amountUSDT) external {
+    function prueba1(address _tokenAddress, uint256 _amountTk) external {
+        
+        if (!isToken[_tokenAddress]) {
+                isToken[_tokenAddress] = true;
+                tokensData[_tokenAddress] = IERC20(_tokenAddress);
+        }
         // approve
-        token1.transferFrom(msg.sender, address(this), _amountUSDT);
+        bool result = tokensData[_tokenAddress].transferFrom(msg.sender, address(this), _amountTk);
+
+        console.log(result); 
+
     }
 
-    function prueba2() external view returns (uint256 sup1, uint256 sup2){
-        return (token1.totalSupply(), token2.totalSupply());
+    function prueba2(address _tokenAddress) external view returns (uint256 reserves){
+        return (tokensData[_tokenAddress].balanceOf(address(this)));
     }
 
     // 1
@@ -74,19 +80,42 @@ contract SimpleSwap is Ownable {
 
     // 4
     function getPrice(address tokenA, address tokenB) external view 
-                returns (uint price){
+                returns (uint256 price){
             //return price;
+            uint256 amountOutA = _getAmountOut2(1*(10**18), tokenB, tokenA);
+            price = 1*(10**18) * 1*(10**18) / amountOutA ; // por 1B me dan xA -> Para comprar 1A -> xB = (1A * 1B) / xA
+            return price;
     }
 
 
     // 5
-    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure 
-                    returns (uint amountOut){
+    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut) external pure 
+                    returns (uint256 amountOut){
+
+            amountOut = (amountIn * reserveOut) / (reserveIn + amountIn);
 
     }
 
 
 
+/**** FUNCIONES AUXILIARES ****/
+
+
+    function _getAmountOut2(uint256 _amountIn, address _addressA, address _addressB) public view
+                    returns (uint256 amountOut){
+            // amountOut = (_amountIn * tokenData[_addressB].reserves) / (tokenData[_addressA].reserves + _amountIn);
+            amountOut = (_amountIn * tokensData[_addressB].balanceOf(address(this))) / (tokensData[_addressA].balanceOf(address(this)) + _amountIn);
+            return amountOut;
+    }
+
+
+    function _getPrice2(address _addressA, address _addressB) public view 
+                returns (uint256 price){ //return price;  //ENUNCIADO: Precio de tokenA en términos de tokenB.
+             
+            uint256 amountOutA = _getAmountOut2(1*(10**18), _addressB, _addressA);
+            price = 1*(10**18) * 1*(10**18) / amountOutA ; // por 1B me dan xA -> Para comprar 1A -> xB = (1A * 1B) / xA
+            return price;
+    }
 
 
 }
